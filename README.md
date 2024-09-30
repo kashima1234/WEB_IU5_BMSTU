5ee9e87651af9533597e924dfa5725515c013c38
kashimaa <kashimaahmed@gmail.com>
2024-09-30 03:02:07 +0300

    ddelold

--- README.md
+++
@@ -1,5 +0,0 @@
-# BMSTU IU5 Laboratory Works.
-
-<div>
-<img src="https://img.shields.io/badge/language-Django-blue.svg" alt="Django Language">
-</div>
--- VikingsServer/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', index),
-    path('places/<int:place_id>/', place),
-    path('expeditions/<int:expedition_id>/', expedition),
-]
